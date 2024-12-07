b01889a71d0c9b55bd186a10909cf31ab1b63109
DayatBang <yadattullah@gmail.com>
2024-12-07 21:07:50 +0700

    membuat Ui profile Edit

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,7 @@ Route::get('answers/1', function () {
 Route::get('users/samsu', function () {
     return view('pages.users.show');
 })->name('users.show');
+
+Route::get('users/samsu/edit', function () {
+    return view('pages.users.form');
+})->name('users.edit');
